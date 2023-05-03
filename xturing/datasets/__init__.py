a1e6ac40b232c23b443012cffaafeb28b3943a62
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-05-03 23:36:44 +0900

    fix error

--- xturing/datasets/__init__.py
+++ xturing/datasets/__init__.py
@@ -2,7 +2,6 @@ from .base import BaseDataset
 from .instruction_dataset import InstructionDataset, InstructionDatasetMeta
 from .text2image_dataset import Text2ImageDataset
 from .text_dataset import TextDataset, TextDatasetMeta
-from .example_dataset import ExampleDataset
 
 BaseDataset.add_to_registry(TextDataset.config_name, TextDataset)
 BaseDataset.add_to_registry(InstructionDataset.config_name, InstructionDataset)
