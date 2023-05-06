77f400001a1e8c216e00f4cb59e58b24d350bffe
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-05-07 00:15:45 +0900

    .

--- xturing/preprocessors/__init__.py
+++ xturing/preprocessors/__init__.py
@@ -1,3 +1,3 @@
 from .base import BasePreprocessor
-from .visual_instruction_collator import InstructionDataCollator
+#from .visual_instruction_collator import InstructionDataCollator
 from .text_collator import TextDataCollator
