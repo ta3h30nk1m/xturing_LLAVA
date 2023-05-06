661141a40e64baf2e9dd9b2f02a2c9bfe2b8bc8f
lsh-159 <110099021+lsh-159@users.noreply.github.com>
2023-05-06 23:58:57 +0900

    Update llama_engine.py
    undo

--- xturing/engines/llama_engine.py
+++ xturing/engines/llama_engine.py
@@ -148,7 +148,8 @@ class LlamaLoraInt4Engine(CausalLoraEngine):
         torch.set_default_dtype(torch.half)
         transformers.modeling_utils._init_weights = False
         #torch.set_default_dtype(torch.half)
-        model = LlamaForCausalLM.from_pretrained("Aitrepreneur/vicuna-7B-1.1-GPTQ-4bit-128g") ### changed 
+        model = LlamaForCausalLM(config)
+        #model = LlamaForCausalLM.from_pretrained("Aitrepreneur/vicuna-7B-1.1-GPTQ-4bit-128g") ### changed 
         torch.set_default_dtype(torch.float)
         model = model.eval()
 
