b3cdbdd43a2e1a6b769e87c2664eaeb53c17681c
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-05-07 00:13:32 +0900

    .

--- xturing/datasets/__init__.py
+++ xturing/datasets/__init__.py
@@ -1,10 +1,10 @@
 from .base import BaseDataset
-from .visual_instruction_dataset import InstructionDataset, InstructionDatasetMeta
+#from .visual_instruction_dataset import InstructionDataset, InstructionDatasetMeta
 from .text2image_dataset import Text2ImageDataset
 from .text_dataset import TextDataset, TextDatasetMeta
 
 # from .visual_instruction_dataset import 
 
 BaseDataset.add_to_registry(TextDataset.config_name, TextDataset)
-BaseDataset.add_to_registry(InstructionDataset.config_name, InstructionDataset)
+#BaseDataset.add_to_registry(InstructionDataset.config_name, InstructionDataset)
 BaseDataset.add_to_registry(Text2ImageDataset.config_name, Text2ImageDataset)
