d7bf0a97193bf7641f45942f5bc5426e8a35012f
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-05-07 00:14:37 +0900

    test

--- xturing/__init__.py
+++ xturing/__init__.py
@@ -2,7 +2,7 @@ from .utils.external_loggers import configure_external_loggers
 
 configure_external_loggers()
 
-from .datasets import BaseDataset, InstructionDataset, TextDataset
+from .datasets import BaseDataset, TextDataset #InstructionDataset
 from .engines import (
     BaseEngine,
     GPT2Engine,
