b2d14cb8c90a6c0252a001bb8cb0e896335410bc
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-05-07 00:18:19 +0900

    test

--- xturing/preprocessors/base.py
+++ xturing/preprocessors/base.py
@@ -1,5 +1,5 @@
 from xturing.models.stable_diffusion import StableDiffusion
-from xturing.preprocessors.instruction_collator import InstructionDataCollator
+#from xturing.preprocessors.instruction_collator import InstructionDataCollator
 from xturing.preprocessors.text_collator import TextDataCollator
 from xturing.registry import BaseParent
 
@@ -8,7 +8,7 @@ class BasePreprocessor(BaseParent):
     registry = {}
 
 
-BasePreprocessor.add_to_registry(
-    InstructionDataCollator.config_name, InstructionDataCollator
-)
+# BasePreprocessor.add_to_registry(
+#     InstructionDataCollator.config_name, InstructionDataCollator
+# )
 BasePreprocessor.add_to_registry(TextDataCollator.config_name, TextDataCollator)
