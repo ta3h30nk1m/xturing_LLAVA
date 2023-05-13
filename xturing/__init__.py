d08646c5b662dd8e32c192740ac7e6caf90af43b
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-05-13 22:53:47 +0900

    fix err

--- xturing/__init__.py
+++ xturing/__init__.py
@@ -5,12 +5,8 @@ configure_external_loggers()
 from .datasets import BaseDataset, TextDataset, InstructionDataset
 from .engines import (
     BaseEngine,
-    GPT2Engine,
-    GPT2LoraEngine,
-    GPTJEngine,
-    GPTJLoraEngine,
     LLamaEngine,
     LlamaLoraEngine,
 )
-from .models import GPT2, BaseModel, GPT2Lora, GPTJLora, Llama, LlamaLora
+from .models import BaseModel, Llama, LlamaLora
 from .trainers import BaseTrainer, LightningTrainer
