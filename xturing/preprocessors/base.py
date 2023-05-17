dee190f647f836c7a5c3d7e2bb29568343482af4
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-05-17 14:38:29 +0900

    add to registry

--- xturing/preprocessors/base.py
+++ xturing/preprocessors/base.py
@@ -1,5 +1,6 @@
 from xturing.preprocessors.instruction_collator import InstructionDataCollator
 from xturing.preprocessors.text_collator import TextDataCollator
+from xturing.preprocessors.llava_instruct_collator import Llava_InstructionDataCollator
 from xturing.registry import BaseParent
 
 class BasePreprocessor(BaseParent):
@@ -10,3 +11,5 @@ BasePreprocessor.add_to_registry(
     InstructionDataCollator.config_name, InstructionDataCollator
 )
 BasePreprocessor.add_to_registry(TextDataCollator.config_name, TextDataCollator)
+
+BasePreprocessor.add_to_registry(Llava_InstructionDataCollator.config_name, Llava_InstructionDataCollator)
